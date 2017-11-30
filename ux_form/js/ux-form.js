ddb4d0d5cdb199d6eaefcffe35504a4f17a41dab
JaceRider <jacerider@gmail.com>
2017-11-30 13:20:19 -0600

    Compiled files.

--- ux_form/js/ux-form.js
+++ ux_form/js/ux-form.js
@@ -1,1 +1,1 @@
-!function(t,e){"use strict";e.behaviors.uxForm={attach:function(t){this.setLastElement(t)},setLastElement:function(t){}},t(document).on("state:visible",function(e){e.trigger&&t(e.target).closest(".ux-form-element-js").toggle(e.value)})}(jQuery,Drupal);
+!function(t,e){"use strict";e.behaviors.uxForm={attach:function(t){},setLastElement:function(t){}},t(document).on("state:visible",function(e){e.trigger&&t(e.target).closest(".ux-form-element-js").toggle(e.value)})}(jQuery,Drupal);
