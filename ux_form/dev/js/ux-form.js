4746569750dc52089c1bf9e1efd343e387446387
JaceRider <jacerider@gmail.com>
2018-02-01 10:05:52 -0600

    Add classes to last visible item in form-wrappers.

--- ux_form/dev/js/ux-form.js
+++ ux_form/dev/js/ux-form.js
@@ -1,27 +1,31 @@
-(function ($, Drupal) {
+(function ($, Drupal, debounce) {
 
   'use strict';
 
+  var timer;
+  function setLastElement(context) {
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+      $('.form-wrapper > .ux-for-element-last').removeClass('ux-for-element-last');
+      $('.field--name-field-ethnicity.form-wrapper').each(function () {
+        $(this).find('> :visible').last().addClass('ux-for-element-last');
+      });
+    }, 100);
+  }
+
   Drupal.behaviors.uxForm = {
     attach: function (context) {
-      // $('.ux-form-element-js')
-      // this.setLastElement(context);
-    },
-
-    setLastElement: function (context) {
-      // var $context = $(context);
-      // $context.find('.ux-form-container-js').each(function () {
-      //   $(this).find('.ux-form-element-js').removeClass('ux-form-element-last').filter(':visible:last').addClass('ux-form-element-last');
-      // });
+      setLastElement();
     }
   };
 
   var $document = $(document);
   $document.on('state:visible', function (e) {
     if (e.trigger) {
+      setLastElement();
       // Integrate with state API to hide parent wrapper elements.
       $(e.target).closest('.ux-form-element-js').toggle(e.value);
     }
   });
 
-})(jQuery, Drupal);
+})(jQuery, Drupal, Drupal.debounce);
